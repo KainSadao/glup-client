ede99dafb5df349e4a1edc85d82d216c111070e1
Kyu <rx1914@hotmail.com>
2016-10-10 21:58:08 -0500

    Inflateview

--- app/src/main/java/com/glup/client/Activities/MainActivity.java
+++ app/src/main/java/com/glup/client/Activities/MainActivity.java
@@ -1,15 +1,38 @@
 package com.glup.client.Activities;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.glup.client.Api.Glup;
 import com.glup.client.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pd = new ProgressDialog(this);
+        pd.setMessage("Cargando productos...");
+        pd.show();
+       /* Glup.getAllProducts(new Response.Listener() {
+            @Override
+            public void onResponse(Object response) {
+                pd.dismiss();
+                Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                pd.dismiss();
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });*/
     }
 }
